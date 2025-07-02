2cf99071531d32e3ae52098f3e55ccce0d973ecd
liammmcb <lm65@rice.edu>
2025-07-02 12:47:32 -0500

    renamed other parts of Trak toVault, changed color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Tracker from './pages/Tracker'; // make sure this is here too
+import Vault from './pages/Vault'; // make sure this is here too
 import { useState } from 'react';
 
 
@@ -32,7 +32,7 @@ function Navbar() {
           <Link to="/" className={currentPath === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
           <Link to="/about" className={currentPath === '/about' ? 'nav-link active' : 'nav-link'}>About</Link>
           <Link to="/contact" className={currentPath === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
-          <Link to="/tracker" className={currentPath === '/tracker' ? 'nav-link active' : 'nav-link'}>Tracker</Link>
+          <Link to="/vault" className={currentPath === '/vault' ? 'nav-link active' : 'nav-link'}>Vault</Link>
         </div>
       </div>
 
@@ -42,7 +42,7 @@ function Navbar() {
           <Link to="/" onClick={toggleMenu} className={currentPath === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
           <Link to="/about" onClick={toggleMenu} className={currentPath === '/about' ? 'nav-link active' : 'nav-link'}>About</Link>
           <Link to="/contact" onClick={toggleMenu} className={currentPath === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
-          <Link to="/tracker" onClick={toggleMenu} className={currentPath === '/tracker' ? 'nav-link active' : 'nav-link'}>Tracker</Link>
+          <Link to="/vault" onClick={toggleMenu} className={currentPath === '/vault' ? 'nav-link active' : 'nav-link'}>Vault</Link>
         </div>
       )}
     </nav>
@@ -57,7 +57,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/tracker" element={<Tracker />} />
+        <Route path="/vault" element={<Vault />} />
       </Routes>
     </Router>
   );
