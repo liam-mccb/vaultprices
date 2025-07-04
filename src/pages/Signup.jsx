acd24524e85a2eef0be1d2e9802041a9fe476589
liammmcb <lm65@rice.edu>
2025-07-03 21:41:35 -0500

    added secure password requirements

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -35,7 +35,16 @@ export default function Signup() {
       {err && <p className="error">{err}</p>}
       <form onSubmit={handleSubmit}>
         <input type="email"    placeholder="Email"    value={email}    onChange={e => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+        <input
+        type="password"
+        placeholder="Password"
+        className="border px-2 py-1 w-full"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        required
+        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,20}$"
+        title="8-20 chars, 1 upper, 1 lower, 1 number, 1 special"
+        />
         <button>Sign up</button>
       </form>
       <p>Have an account? <Link to="/login">Log in</Link></p>
